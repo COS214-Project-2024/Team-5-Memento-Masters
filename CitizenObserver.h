93aec2f2f76f4d137b13f72ccb73d7ee5f5193bf
Corne <u23788862@tuks.co.za>
2024-10-31 20:10:47 +0200

    Started to add some basic doxygen comments for Observers

--- CitizenObserver.h
+++ CitizenObserver.h
@@ -5,19 +5,35 @@
 #include <vector>
 #include "Citizen.h"
 
+/**
+ * @brief Concrete observer class that observes changes in the city and updates citizens
+ */
 class CitizenObserver : public CityObserver {
     private:
          std::vector<Citizen*> citizens; ///< List of citizens in the city
     protected:
         // std::vector<Citizen*> citizens;
     public:
+        /**
+         * @brief Construct a new CitizenObserver object
+         */
         CitizenObserver();
 
+        /**
+         * @brief Destroy the CitizenObserver object
+         */
         ~CitizenObserver();
         // virtual void update();
-
         //virtual void update(bool isTaxIncrease) ;
-         virtual void update(const std::string& taxType, double newRate);
+
+        /**
+         * @brief Updates the citizens mood based on the new tax rate
+         * 
+         * @param taxType The type of tax that has changed
+         * @param newRate The new tax rate
+         * @return * void 
+         */
+        virtual void update(const std::string& taxType, double newRate);
 };
 
 #endif
--- CityObserver.h
+++ CityObserver.h
@@ -5,22 +5,41 @@
 #include <string>
 class City;
 
+/**
+ * @brief Abstract class representing an observer for the City class
+ */
 class CityObserver { 
     private:
 
     protected:
-        City* subject; 
+        City* subject; ///< The city being observer (the subject)
     public:
-
+        /**
+         * @brief Construct a new CityObserver object
+         */
         CityObserver();
 
+        /**
+         * @brief Destroy the CityObserver object
+         */
         virtual ~CityObserver();
 
         // virtual void update() = 0;
-
         // virtual void update(bool isTaxIncrease) = 0;
+
+        /**
+         * @brief Update the observer with the new tax rate
+         * 
+         * @param taxType The type of tax that has changed
+         * @param newRate The new tax rate
+         */
         virtual void update(const std::string& taxType, double newRate) = 0;
 
+        /**
+         * @brief Set the Subject (city) to be observed
+         * 
+         * @param subject The city to be observed
+         */
         virtual void setSubject(City* subject) ;
 };
 
