b80704ea244c05684938cbb9cf0ccfa542690f64
Michael Todd <michaeltodd981@gmail.com>
2024-11-01 09:29:30 +0200

    Added Prodcution main - We will be using this when testing is basically done

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// THIS IS BASICALLY OUR PRODUCTION MAIN...
+// ...AND WE'RE GOING TO USE IT TO TEST OUR CODE
+// ...AND MAKE SURE IT WORKS AS EXPECTED
+
+// THIS IS WHERE WE ARE HAPPY WITH OUR CODE AND TESTING WE CAN ADD IT TO OUR PRODUCTION CODE
+
+int main() {
+    std::cout << "Welcome to the City of Michael's Communist Rule!" << std::endl;
+
+    
+    // Initialize your components here
+    //
+    //
+    //
+
+    // Add your production code here once you are happy with the testing
+    //
+    //
+    //
+
+    return 0;
+}
