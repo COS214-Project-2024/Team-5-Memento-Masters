49edb1632ab1faf8420ec480116f85896cc0aaf8
u22593048 <u22593048@tuks.co.za>
2024-11-01 08:14:23 +0200

    feat(citizen, mood and testing): adding doxygen docs for citizenMood

--- CitizenMood/ExcellentMood.h
+++ CitizenMood/ExcellentMood.h
@@ -1,22 +1,38 @@
-//
-// Created by USER-PC on 2024/10/31.
-//
-
 #ifndef MEMENTOMASTERS_EXCELLENTMOOD_H
 #define MEMENTOMASTERS_EXCELLENTMOOD_H
 
 #include "CitizenMood.h"
 
-class ExcellentMood: public CitizenMood{
+/**
+ * @class ExcellentMood
+ * @brief Represents the excellent mood of a citizen.
+ *
+ * Inherits from CitizenMood and defines transitions to AverageMood and PoorMood.
+ */
+class ExcellentMood: public CitizenMood {
 public:
+    /**
+     * @brief Constructs an ExcellentMood object with default health value.
+     */
     ExcellentMood();
 
+    /**
+     * @brief Keeps the citizen's mood in ExcellentMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isExcellent(Citizen *c) override;
 
+    /**
+     * @brief Transitions the citizen's mood to AverageMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isAverage(Citizen *c) override;
 
+    /**
+     * @brief Transitions the citizen's mood to PoorMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isPoor(Citizen *c) override;
 };
-
 
 #endif //MEMENTOMASTERS_EXCELLENTMOOD_H
--- CitizenMood/PoorMood.h
+++ CitizenMood/PoorMood.h
@@ -1,22 +1,38 @@
-//
-// Created by USER-PC on 2024/10/31.
-//
-
 #ifndef MEMENTOMASTERS_POORMOOD_H
 #define MEMENTOMASTERS_POORMOOD_H
 
 #include "CitizenMood.h"
-class PoorMood: public CitizenMood{
+
+/**
+ * @class PoorMood
+ * @brief Represents the poor mood of a citizen.
+ *
+ * Inherits from CitizenMood and defines transitions to AverageMood and ExcellentMood.
+ */
+class PoorMood: public CitizenMood {
 public:
+    /**
+     * @brief Constructs a PoorMood object with default health value.
+     */
     PoorMood();
 
-public:
+    /**
+     * @brief Transitions the citizen's mood to ExcellentMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isExcellent(Citizen *c) override;
 
+    /**
+     * @brief Transitions the citizen's mood to AverageMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isAverage(Citizen *c) override;
 
+    /**
+     * @brief Keeps the citizen's mood in PoorMood.
+     * @param c Pointer to the Citizen object.
+     */
     void isPoor(Citizen *c) override;
 };
-
 
 #endif //MEMENTOMASTERS_POORMOOD_H
