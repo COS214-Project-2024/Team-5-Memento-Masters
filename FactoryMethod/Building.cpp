2a238625853701cceae63a48327cc21df6fc2fbb
Michael Todd <michaeltodd981@gmail.com>
2024-11-04 19:18:28 +0200

    Updated things so program would compile with new Visitor added - Still needs work from Keegan and Dean

--- FactoryMethod/Building.cpp
+++ FactoryMethod/Building.cpp
@@ -1,33 +1,33 @@
-
 #include <vector>
+#include "Building.h"
+#include "../Visitor/BuildingReport.h" // Include the definition of BuildingReport
+
 using namespace std;
 
-#include "Building.h"
-
 void Building::removeCitizen(Citizen* citizen) {
-	throw "Not yet implemented";
+    throw "Not yet implemented";
 }
 
 void Building::addCitizen(Citizen* citizen) {
-	throw "Not yet implemented";
+    throw "Not yet implemented";
 }
 
 void Building::consumeResources() {
-	throw "Not yet implemented";
+    throw "Not yet implemented";
 }
 
 void Building::buy(Citizen* newOwner) {
-	throw "Not yet implemented";
+    throw "Not yet implemented";
 }
 
 void Building::accept(BuildingReport* v) {
-v->visit(this);
+    v->visit(this);
 }
 
 void Building::accept(Visitor* v) {
-	throw "Not yet implemented";
+    throw "Not yet implemented";
 }
 
-int Building::getCost(){
+int Building::getCost() {
     return cost;
 }
--- FactoryMethod/Building.h
+++ FactoryMethod/Building.h
@@ -1,48 +1,85 @@
-#include "Visitor/BuildingReport.h"
-#include <vector>
-#include <string>
-using namespace std;
-
 #ifndef BUILDING_H
 #define BUILDING_H
 
-#include "UtilState.h"
-class Visitor;  //TODO replace with //#include "Visitor.h"
-class Citizen;  //TODO replace with //#include "Citizen.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Visitor;
+class Citizen;
 class UtilState;
+class BuildingReport;
 
+/**
+ * @class Building
+ * @brief Abstract base class for buildings.
+ */
+class Building {
+private:
+    int cost; ///< Cost of the building.
+    vector<Citizen*> currentOccupants; ///< Current occupants of the building.
+    vector<Citizen*> owner; ///< Owners of the building.
+    string type; ///< Type of the building.
+    UtilState* buildingState; ///< State of the building.
 
-class Building
-{
-private: 
-    int cost;
-    vector<Citizen*> currentOccupants;
-    vector<Citizen*> owner;
-    string type;
-    UtilState* buildingState;
-
-public: 
+public:
+    /**
+     * @brief Constructor for Building.
+     * @param cost Cost of the building.
+     * @param type Type of the building.
+     */
     Building(int cost = 0, const std::string& type = "") : cost(cost), type(type) {}
 
+    /**
+     * @brief Removes a citizen from the building.
+     * @param citizen Pointer to the citizen to remove.
+     */
     void removeCitizen(Citizen* citizen);
 
+    /**
+     * @brief Adds a citizen to the building.
+     * @param citizen Pointer to the citizen to add.
+     */
     void addCitizen(Citizen* citizen);
 
+    /**
+     * @brief Consumes resources for the building.
+     */
     void consumeResources();
 
+    /**
+     * @brief Buys the building for a new owner.
+     * @param newOwner Pointer to the new owner.
+     */
     void buy(Citizen* newOwner);
 
+    /**
+     * @brief Accepts a visitor to perform operations on the building.
+     * @param v Pointer to the Visitor.
+     */
     void accept(Visitor* v);
 
+    /**
+     * @brief Accepts a building report visitor to generate a report.
+     * @param v Pointer to the BuildingReport visitor.
+     */
+    void accept(BuildingReport* v);
+
+    /**
+     * @brief Gets the cost of the building.
+     * @return The cost of the building.
+     */
     int getCost();
 
-void accept(BuildingReport* v);
-
+    /**
+     * @brief Destructor for Building.
+     */
     virtual ~Building() {
         // delete buildingState;
         // currentOccupants.clear();
         // owner.clear();
     }
-};  
+};
 
-#endif //BUILDING_H
+#endif // BUILDING_H
