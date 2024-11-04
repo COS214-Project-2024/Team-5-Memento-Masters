4bfd039f80b8025fcb3d4c375d9e4b9a9e21c693
K33gan123 <127729270+K33gan123@users.noreply.github.com>
2024-11-04 19:52:10 +0200

    Update RoadReport.h

--- Visitor/RoadReport.h
+++ Visitor/RoadReport.h
@@ -1,19 +1,23 @@
 #ifndef ROADREPORT_H
 #define ROADREPORT_H
 #include "Visitor.h"
+#include "FactoryMethod/Road.h"
 #include <string>
-#pragma once
-
+class Road;
 class RoadReport : public Visitor
 {
 public:
     RoadReport();
+    /// @brief Generates a report of the City's Roads overall efficiency
+    /// @return A string output of average efficiency
     std::string generateReport() override;
+    /// @brief Uses the passed in parameter to calculate the total sum of Efficiency
+    /// @param road Used to sum total efficiency within the City
     void visit(Road *road);
     ~RoadReport();
 
 private:
-int efficiencySum=50;
+int efficiencySum;
 };
 
 #endif
