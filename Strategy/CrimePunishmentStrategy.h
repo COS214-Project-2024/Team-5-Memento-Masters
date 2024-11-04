34e2c76fcab6e664a5cc2ae2acab8fa15e85b7e6
angiexbreedt <u23542838@tuks.co.za>
2024-11-04 14:47:06 +0200

    Update CrimePunishmentStrategy.h
    latest version of CrimePunishmentStrategy.h

--- Strategy/CrimePunishmentStrategy.h
+++ Strategy/CrimePunishmentStrategy.h
@@ -1,6 +1,9 @@
 // CrimePunishmentStrategy.h
 // STRATEGY DESIGN PATTERN (Strategy)
 
+// CrimePunishmentStrategy.h
+// STRATEGY DESIGN PATTERN (Strategy)
+
 #ifndef CRIME_PUNISHMENT_STRATEGY_H
 #define CRIME_PUNISHMENT_STRATEGY_H
 
@@ -9,11 +12,19 @@
 class CrimePunishmentStrategy {
 public:
     virtual ~CrimePunishmentStrategy();
-    virtual std::string punish() const = 0;
     virtual std::string punishCrime(const std::string& crime) const = 0; 
 };
 
 #endif // CRIME_PUNISHMENT_STRATEGY_H
+
+
+
+
+
+
+
+
+
 
 
 
