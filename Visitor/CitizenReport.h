861342489dc3a3cde3f608ec47e50e2d2833414a
K33gan123 <127729270+K33gan123@users.noreply.github.com>
2024-11-04 19:52:32 +0200

    Update CitizenReport.h

--- Visitor/CitizenReport.h
+++ Visitor/CitizenReport.h
@@ -1,13 +1,20 @@
 #ifndef CITIZENREPORT_H
 #define CITIZENREPORT_H
 #include "Visitor.h"
-#pragma once
+#include "Citizen.h"
+#include "CitizenMood/ExcellentMood.h"
+#include "CitizenMood/AverageMood.h"
+class Citizen;
 
 class CitizenReport : public Visitor
 {
 public:
     CitizenReport();
+    /// @brief Generates a report of the overall Citizens average mood and health
+/// @return A string output of the average mood and health
     std::string generateReport() override;
+    /// @brief Uses the provided passed in citizen to tally the Sum of mood and health values 
+    /// @param citizen Used to calculate the average Mood and Health of all Citizens
       void visit(Citizen* citizen);
     ~CitizenReport();
 
