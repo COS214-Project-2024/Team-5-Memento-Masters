3b1a61bcf44a53c28a1b0fbbcd9c33c938d2d7ae
K33gan123 <127729270+K33gan123@users.noreply.github.com>
2024-11-04 23:51:29 +0200

    Update Road.h

--- FactoryMethod/Road.h
+++ FactoryMethod/Road.h
@@ -1,15 +1,19 @@
 
 #ifndef ROAD_H
 #define ROAD_H
-
+#include "../Visitor/RoadReport.h";
 #include "TransportBuilding.h"
 
 class Road: public TransportBuilding
 {
 public:
-    Road(int cost = 10000) : TransportBuilding(cost, 100) {}
-        void accept(RoadReport *v);
-    ~Road() = default;
+ Road(int cost = 10000) : TransportBuilding(cost, 100) {}
+  void accept(RoadReport* v);
+  int getEfficiencyValue();
+    ~Road();
+~Road() = default;
+private:
+int efficiencyValue;
 };
 
 #endif
