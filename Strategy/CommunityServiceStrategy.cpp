4de0bdae74598229ddcf5f8a9d6da62ca5724ddc
angiexbreedt <u23542838@tuks.co.za>
2024-11-04 14:44:41 +0200

    Update CommunityServiceStrategy.cpp
    latest version of CommunityServiceStrategy.cpp

--- Strategy/CommunityServiceStrategy.cpp
+++ Strategy/CommunityServiceStrategy.cpp
@@ -3,10 +3,6 @@
 
 #include "CommunityServiceStrategy.h"
 
-std::string CommunityServiceStrategy::punish() const {
-    return "Assigned to community service.";
-}
-
 std::string CommunityServiceStrategy::punishCrime(const std::string& crime) const {
-    return "For the crime of " + crime + ", assigned to community service.";
+    return "Punishment for " + crime + " : 100 hours of community service.";
 }
