8631b947bca7f10158d63ddeaf55a33fde45cf34
angiexbreedt <u23542838@tuks.co.za>
2024-11-04 16:15:39 +0200

    Update Citizen.cpp
    latest version of Citizen.cpp

--- Citizen.cpp
+++ Citizen.cpp
@@ -4,8 +4,11 @@
 #include "Strategy/DeathSentenceStrategy.h"
 #include <stdexcept>
 
-Citizen::Citizen(const std::string &name, int age, bool hasCriminalRecord, const std::string &jobTitle, std::unique_ptr<CrimePunishmentStrategy> strategy)
-    : name(name), age(age), hasCriminalRecord(hasCriminalRecord), jobTitle(jobTitle), mood(nullptr), strategy(std::move(strategy)) {}
+}
+Citizen::Citizen(const string &name, int age, const string &jobTitle) //updated
+    : name(name), jobTitle(jobTitle), 
+      mood(NULL), punishmentStrategy(NULL) {}
+
 
 void Citizen::setMood(CitizenMood *mood) {
     this->mood = mood;
@@ -32,14 +35,6 @@ void Citizen::setAge(int age) {
     Citizen::age = age;
 }
 
-bool Citizen::isHasCriminalRecord() const {
-    return hasCriminalRecord;
-}
-
-void Citizen::setHasCriminalRecord(bool hasCriminalRecord) {
-    Citizen::hasCriminalRecord = hasCriminalRecord;
-}
-
 const string &Citizen::getJobTitle() const {
     return jobTitle;
 }
@@ -47,6 +42,11 @@ const string &Citizen::getJobTitle() const {
 void Citizen::setJobTitle(const string &jobTitle) {
     Citizen::jobTitle = jobTitle;
 }
+
+bool Citizen::hasCriminalRecord() const { //updated
+    return !crimes.empty();
+}
+
 
 const vector<string> &Citizen::getCrimes() const {
     return crimes;
@@ -60,28 +60,75 @@ int Citizen::calculateHealth() {
     return this->mood->getHealth();
 }
 
+
 // Complaint handling (Chain of Responsibility)
 void Citizen::makeComplaint(string complaint) {
     // I need to still integrate the Chain of Responsibility pattern - (Michael)
 }
 
+
 // Crime and punishment (Strategy Pattern)
-void Citizen::commitCrime(const string& crime) {
-    if (crime == "petty") {
-        setPunishmentStrategy(make_unique<CommunityServiceStrategy>());
-    } else if (crime == "serious") {
-        setPunishmentStrategy(make_unique<PrisonStrategy>());
-    } else if (crime == "murder") {
-        setPunishmentStrategy(make_unique<DeathSentenceStrategy>());
-    } else {
-        throw invalid_argument("Unknown crime type");
+void Citizen::setPunishmentStrategy(CrimePunishmentStrategy* strategy) { //new
+    delete punishmentStrategy;
+    punishmentStrategy = strategy;
+}
+
+string Citizen::punish(const string& crime) { //new
+    if (punishmentStrategy != NULL) {
+        delete punishmentStrategy;
     }
+    
+    if (crime == "A" || crime == "murder" || crime == "Murder") {
+        punishmentStrategy = new DeathSentenceStrategy();
+    }
+    else if (crime == "B" ||crime == "serious crime" || crime == "Serious Crime" || crime == "robbery" || crime == "Robbery") {
+        punishmentStrategy = new PrisonStrategy();
+    }
+    else if (crime == "C" ||crime == "petty crime" || crime == "Petty Crime" || crime == "vandalism" || crime == "Vandalism") {
+        punishmentStrategy = new CommunityServiceStrategy();
+    }
+    else {
+        return "Unknown crime type: " + crime + ". No punishment.";
+    }
+
+    return punishmentStrategy->punishCrime(crime);
 }
 
-void Citizen::setPunishmentStrategy(unique_ptr<CrimePunishmentStrategy> newStrategy) {
-    strategy = move(newStrategy);
+void Citizen::addCrime(const string& crime) {  //new
+    crimes.push_back(crime);
 }
 
-string Citizen::receivePunishmentForCrime(const string& crime) const {
-    return strategy ? strategy->punishCrime(crime) : "No punishment strategy set.";
-}
+
+// OLD STARTEGY METHODS (revert back if neccessary):
+
+// Citizen::Citizen(const std::string &name, int age, bool hasCriminalRecord, const std::string &jobTitle, std::unique_ptr<CrimePunishmentStrategy> strategy)
+//     : name(name), age(age), hasCriminalRecord(hasCriminalRecord), jobTitle(jobTitle), mood(nullptr), strategy(std::move(strategy)) {
+
+
+// void Citizen::commitCrime(const string& crime) {
+//     if (crime == "petty") {
+//         setPunishmentStrategy(make_unique<CommunityServiceStrategy>());
+//     } else if (crime == "serious") {
+//         setPunishmentStrategy(make_unique<PrisonStrategy>());
+//     } else if (crime == "murder") {
+//         setPunishmentStrategy(make_unique<DeathSentenceStrategy>());
+//     } else {
+//         throw invalid_argument("Unknown crime type");
+//     }
+// }
+
+// void Citizen::setPunishmentStrategy(unique_ptr<CrimePunishmentStrategy> newStrategy) {
+//     strategy = move(newStrategy);
+// }
+
+// string Citizen::receivePunishmentForCrime(const string& crime) const {
+//     return strategy ? strategy->punishCrime(crime) : "No punishment strategy set.";
+// }
+
+// bool Citizen::isHasCriminalRecord() const {  
+//     return hasCriminalRecord;
+// }
+
+// void Citizen::setHasCriminalRecord(bool hasCriminalRecord) {
+//     Citizen::hasCriminalRecord = hasCriminalRecord;
+// }
