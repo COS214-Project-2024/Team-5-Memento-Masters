537759c52b925514c5cc43cae9df3279f3cf7a24
Corne <u23788862@tuks.co.za>
2024-11-05 04:14:01 +0200

    Doxygen comments for Strategy

--- Strategy/DeathSentenceStrategy.h
+++ Strategy/DeathSentenceStrategy.h
@@ -6,8 +6,17 @@
 
 #include "CrimePunishmentStrategy.h"
 
+/**
+ * @class DeathSentenceStrategy
+ * @brief Concrete strategy class for punishing crimes with the death sentence
+ */
 class DeathSentenceStrategy : public CrimePunishmentStrategy {
 public:
+    /**
+     * @brief Construct a new Death Sentence Strategy object
+     * @param crime The crime to be punished
+     * @return A new Death Sentence Strategy object
+     */
     std::string punishCrime(const std::string& crime) const override;
 };
 
